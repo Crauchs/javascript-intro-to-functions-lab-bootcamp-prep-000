4d6b0c754e6ea56db593c19a26eddc631fba1197
Kenneth Cravens <bombfodder93@gmail.com>
2017-09-06 22:04:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ function shout(string) {return string.toUpperCase()}
 function whisper(string) {return string.toLowerCase()}
 function logShout(string) {console.log(`${string}`.toUpperCase())}
 function logWhisper(string) {console.log(`${string}`.toLowerCase())}
-function sayHiToGrandma(string) {if (string === 'hello') {return "I can't hear you!"}
-else if (string === 'HELLO') {return "YES INDEED!"}
-else if (string === 'I love you, Grandma.') {return "I love you, too."}}
+function sayHiToGrandma(string) {
+  var uppercase="HELLO"
+  var lowercase="HELLO"
+  if (string === lowercase) {return "I can't hear you!"}
+else if (string === uppercase) {return "YES INDEED!"}
+else if {return "I love you, too."}}
