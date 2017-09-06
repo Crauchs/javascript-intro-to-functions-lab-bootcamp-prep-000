92cea19bebc523b85134b10586928f98743faa82
Kenneth Cravens <bombfodder93@gmail.com>
2017-09-06 22:05:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function logShout(string) {console.log(`${string}`.toUpperCase())}
 function logWhisper(string) {console.log(`${string}`.toLowerCase())}
 function sayHiToGrandma(string) {
   var uppercase="HELLO"
-  var lowercase="HELLO"
+  var lowercase="hello"
   if (string === lowercase) {return "I can't hear you!"}
 else if (string === uppercase) {return "YES INDEED!"}
 else if {return "I love you, too."}}
